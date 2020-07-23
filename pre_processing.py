77ee50bc20755ec39bd0a10d034ad4922f4dcaa5
CSteinbeck <colton@steinbeck-home.com>
2020-07-23 17:10:23 -0600

    Updates to the Preprocessing of Articles

--- pre_processing.py
+++ pre_processing.py
@@ -1,6 +1,22 @@
-import nltk
+##Preprocessing Data for each article
+##Colton Steinbeck and Julian Stephens
+import nltk, io
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer 
+from nltk.tokenize import word_tokenize 
+from nltk.tokenize import sent_tokenize 
+##Parses Article into readable parts of the system
+article= open("TestArticle.txt")
+line=article.read()
+word=line.split()
+
+##Initializes Stemming Process# and Stop Words#
+ps= PortStemmer()
+stop_words= set.stopwords(words('english'))
 
 
-def find_features:
-     
+for w in word:
+    word_tokenize(w)
+    ps.stem(w)
+##def find_features():
+
